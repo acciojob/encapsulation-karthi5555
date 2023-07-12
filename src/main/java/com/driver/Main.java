d57b84cc7a141b09a7a02d4315902f88cac3489c
Karthik M <karthikm7483@gmail.com>
2023-07-12 12:02:16 +0530

    changed code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "John"; // Error: 'name' has private access in 'com.driver.RWOnly'
+        // System.out.println(obj.name); // Error: 'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("karthi"); // Set a value to name using the setter method
+        System.out.println(obj.getName()); // Retrieve and print the value of name using the getter method
+    }
 }
